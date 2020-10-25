69b4ce9d1109d31dd5da88274cf62eceb13c367d
Emdien <gonnm8@gmail.com>
2020-10-25 15:50:10 +0100

    Añadidas librerias de persistencia

--- src/main/java/umu/tds/apps/persistencia/AdaptadorUsuarioTDS.java
+++ src/main/java/umu/tds/apps/persistencia/AdaptadorUsuarioTDS.java
@@ -1,5 +1,56 @@
 package umu.tds.apps.persistencia;
 
-public class AdaptadorUsuarioTDS {
+import java.util.List;
 
+import beans.Entidad;
+import beans.Propiedad;
+import tds.driver.FactoriaServicioPersistencia;
+import tds.driver.ServicioPersistencia;
+import umu.tds.apps.modelo.Usuario;
+
+public class AdaptadorUsuarioTDS implements IAdaptadorUsuarioDAO{
+
+	private static ServicioPersistencia servPersistencia;
+	private static AdaptadorUsuarioTDS unicaInstancia = null;
+	
+	public static AdaptadorUsuarioTDS getUnicaInstancia() {
+		if (unicaInstancia == null) return new AdaptadorUsuarioTDS();
+		else return unicaInstancia;
+	}
+	
+	private AdaptadorUsuarioTDS() {
+		servPersistencia = FactoriaServicioPersistencia.getInstance().getServicioPersistencia();
+	}
+	
+	public void registrarUsuario(Usuario user) {
+
+	}
+
+	public void borrarUsuario(Usuario user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void modificarUsuario(Usuario user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Usuario recuperarUsuario(int codigo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Usuario> recuperarTodosUsuarios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+	
+	
+	
+	
+	
 }
