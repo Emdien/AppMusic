79bf5f077f576e383e44fb13dcf2343c0f2dd36f
Emdien <gonnm8@gmail.com>
2021-02-26 20:17:08 +0100

    Refactoring y cleanup del codigo
    - Modificado el código de la ventana principal para que sea más limpio y sencillo
    
    - Añadido una referencia estatica a la carpeta de imagenes
    
    - BUGS: Actualmente hay un bug en el que se crean playlists vacias las cuales no se guardan en persistencia. Este bug ocurre al cancelar la creacion de una playlist

--- src/main/java/umu/tds/apps/vista/Principal.java
+++ src/main/java/umu/tds/apps/vista/Principal.java
@@ -1,6 +1,6 @@
 package umu.tds.apps.vista;
 
-import java.awt.EventQueue;
+
 
 import javax.swing.JFrame;
 import javax.swing.BoxLayout;
@@ -18,7 +18,6 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import java.awt.FlowLayout;
 import javax.swing.Box;
-import javax.swing.border.LineBorder;
 
 import umu.tds.apps.controlador.ControladorAppMusic;
 import umu.tds.apps.modelo.Cancion;
@@ -27,27 +26,23 @@ import umu.tds.apps.modelo.ListaReproduccion;
 import java.awt.Color;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
-import javax.swing.ListSelectionModel;
-import javax.swing.AbstractListModel;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
-import javax.swing.ScrollPaneConstants;
 import javax.swing.event.ListSelectionListener;
 
 import com.itextpdf.text.DocumentException;
 
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.ImageIcon;
-import java.awt.Point;
 import javax.swing.border.MatteBorder;
 import javax.swing.SwingConstants;
 
-public class Principal {
+public class Principal implements ActionListener{
 	
 	
 	private static final int PAUSED = 0;
 	private static final int PLAYING = 1;
 	private int estadoReproductor = PAUSED;
+	private static final String IMG_SRC= "/umu/tds/apps/images/";
 	
 	// Referencia para que otros paneles puedan hacer llamadas a la ventana principal
 	private static Principal instanciaPrincipal;
@@ -80,6 +75,7 @@ public class Principal {
 	private JLabel lblCancionSeleccionada;
 	private JLabel lblSelectedSong;
 	private JLabel lblSelectedArtist;
+	private JButton btnLogout;
 	
 	
 	private ControladorAppMusic controlador;
@@ -120,17 +116,9 @@ public class Principal {
 		header_panel.setPreferredSize(new Dimension(10, 50));
 		frame.getContentPane().add(header_panel);
 		
-		JButton btnLogout = new JButton("Cerrar sesi\u00F3n");
-		btnLogout.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/Logout.png")));
-		btnLogout.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				controlador.logout();
-				Login windowL = new Login();
-				frame.dispose();
-			}
-			
-			
-		});
+		btnLogout = new JButton("Cerrar sesi\u00F3n");
+		btnLogout.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "Logout.png")));
+		btnLogout.addActionListener(this);
 		
 		lblUsuario = new JLabel();
 		lblUsuario.setText(controlador.getUsuarioActual().getNombre());
@@ -140,19 +128,8 @@ public class Principal {
 		header_panel.add(rigidArea_1);
 		
 		btnMejorar = new JButton("Mejorar tu cuenta");
-		btnMejorar.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/Premium.png")));
-		btnMejorar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				scrollPane.setVisible(false);
-				MejorarCuenta panel_mc = new MejorarCuenta(frame);
-				body_panel.remove(current_content);
-				body_panel.add(panel_mc, BorderLayout.CENTER);
-				current_content = panel_mc;
-				frame.repaint();
-				frame.revalidate();
-				//System.out.println("test");
-			}
-		});
+		btnMejorar.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "Premium.png")));
+		btnMejorar.addActionListener(this);
 		header_panel.add(btnMejorar);
 		
 		Component rigidArea = Box.createRigidArea(new Dimension(20, 20));
@@ -180,110 +157,44 @@ public class Principal {
 		
 		btnExplorar = new JButton("Explorar Canciones");
 		btnExplorar.setHorizontalAlignment(SwingConstants.LEFT);
-		btnExplorar.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/Search.png")));
+		btnExplorar.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "Search.png")));
 		btnExplorar.setPreferredSize(new Dimension(225, 50));
 		btnExplorar.setMinimumSize(new Dimension(225, 50));
-		btnExplorar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				scrollPane.setVisible(false);
-				Explorar panel_exp = new Explorar(frame);
-				body_panel.remove(current_content);
-				body_panel.add(panel_exp, BorderLayout.CENTER);
-				current_content = panel_exp;
-				frame.repaint();
-				frame.revalidate();	
-			}
-		});
+		btnExplorar.addActionListener(this);	
 		btnExplorar.setMaximumSize(new Dimension(225, 50));
 		sidebar_buttons.add(btnExplorar);
 		
 		btnNuevaLista = new JButton("Nueva Playlist");
 		btnNuevaLista.setHorizontalAlignment(SwingConstants.LEFT);
-		btnNuevaLista.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/NewList.png")));
+		btnNuevaLista.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "NewList.png")));
 		btnNuevaLista.setPreferredSize(new Dimension(225, 50));
 		btnNuevaLista.setMinimumSize(new Dimension(225, 50));
-		btnNuevaLista.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				scrollPane.setVisible(false);
-				NuevaLista panel_nl = new NuevaLista(frame);
-				body_panel.remove(current_content);
-				body_panel.add(panel_nl, BorderLayout.CENTER);
-				current_content = panel_nl;
-				frame.repaint();
-				frame.revalidate();
-			}
-		});
+		btnNuevaLista.addActionListener(this);
 		btnNuevaLista.setMaximumSize(new Dimension(225, 50));
 		sidebar_buttons.add(btnNuevaLista);
 		
 		btnReciente = new JButton("Canciones Recientes");
 		btnReciente.setHorizontalAlignment(SwingConstants.LEFT);
-		btnReciente.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/Recent.png")));
+		btnReciente.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "Recent.png")));
 		btnReciente.setPreferredSize(new Dimension(225, 50));
 		btnReciente.setMinimumSize(new Dimension(225, 50));
-		btnReciente.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				scrollPane.setVisible(false);
-				Reciente panel_rec = new Reciente(frame);
-				body_panel.remove(current_content);
-				body_panel.add(panel_rec, BorderLayout.CENTER);
-				current_content = panel_rec;
-				frame.repaint();
-				frame.revalidate();
-			}
-		});
+		btnReciente.addActionListener(this);
 		btnReciente.setMaximumSize(new Dimension(225, 50));
 		sidebar_buttons.add(btnReciente);
 		
 		btnMisListas = new JButton("Mis Playlists");
 		btnMisListas.setHorizontalAlignment(SwingConstants.LEFT);
-		btnMisListas.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/MyLists.png")));
+		btnMisListas.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "MyLists.png")));
 		btnMisListas.setPreferredSize(new Dimension(225, 50));
 		btnMisListas.setMinimumSize(new Dimension(225, 50));
-		btnMisListas.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				if (update_playlist_list()) {				// Existen listas
-					list.setSelectedIndex(0);
-					MisListas panel_ml = new MisListas(frame);
-					panel_ml.update_selected_playlist(list.getSelectedIndex());
-					body_panel.remove(current_content);
-					body_panel.add(panel_ml, BorderLayout.CENTER);
-					current_content = panel_ml;
-					frame.repaint();
-					frame.revalidate();
-					
-				} else {
-					
-					JOptionPane.showMessageDialog(frame,
-							"No existe ninguna playlist actualmente",
-							"Error", JOptionPane.ERROR_MESSAGE);
-					
-					
-					
-				}
-				
-				
-			}
-		});
+		btnMisListas.addActionListener(this);
 		btnMisListas.setMaximumSize(new Dimension(225, 50));
 		sidebar_buttons.add(btnMisListas);
 		
 		btnMasReproducidas = new JButton("Canciones mas Populares");
 		btnMasReproducidas.setHorizontalAlignment(SwingConstants.LEFT);
-		btnMasReproducidas.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/Popular.png")));
-		btnMasReproducidas.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				scrollPane.setVisible(false);
-				MasReproducidas panel_mr = new MasReproducidas(frame);
-				body_panel.remove(current_content);
-				body_panel.add(panel_mr, BorderLayout.CENTER);
-				current_content = panel_mr;
-				frame.repaint();
-				frame.revalidate();
-				
-			}
-		});
+		btnMasReproducidas.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "Popular.png")));
+		btnMasReproducidas.addActionListener(this);
 		btnMasReproducidas.setEnabled(false);
 		btnMasReproducidas.setMinimumSize(new Dimension(225, 50));
 		btnMasReproducidas.setMaximumSize(new Dimension(225, 50));
@@ -292,26 +203,8 @@ public class Principal {
 		
 		btnImprimir = new JButton("Imprimir Playlists");
 		btnImprimir.setHorizontalAlignment(SwingConstants.LEFT);
-		btnImprimir.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/Print.png")));
-		btnImprimir.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				JFileChooser chooser = new JFileChooser();
-				chooser.setCurrentDirectory(new java.io.File("."));
-				chooser.setDialogTitle("Selecciona carpeta de destino");
-				chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-				chooser.setAcceptAllFileFilterUsed(false);
-				
-				if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
-					String path = chooser.getSelectedFile().getAbsolutePath();
-					System.out.println(path);
-					try {
-						controlador.printPDF(path);
-					} catch (DocumentException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		});
+		btnImprimir.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "Print.png")));
+		btnImprimir.addActionListener(this);
 		btnImprimir.setEnabled(false);
 		btnImprimir.setPreferredSize(new Dimension(225, 50));
 		btnImprimir.setMinimumSize(new Dimension(225, 50));
@@ -377,100 +270,24 @@ public class Principal {
 		
 		btnPrevious = new JButton("");
 		panel_1.add(btnPrevious);
-		btnPrevious.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/PreviousButton.png")));
-		btnPrevious.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-
-				if (playlistActual != null) {
-					if (playlistIndex == 0) {
-						playlistIndex = playlistActual.getCanciones().size()-1;
-						cancionActual = playlistActual.getCanciones().get(playlistIndex);
-						controlador.reproducirCancion(cancionActual);
-						btnPlay.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/PauseButton.png")));
-						estadoReproductor = PLAYING;
-						lblSelectedSong.setText(cancionActual.getTitulo());
-						lblSelectedArtist.setText(cancionActual.getInterprete());
-						frame.repaint();
-						frame.revalidate();
-					} else {
-						playlistIndex--;
-						cancionActual = playlistActual.getCanciones().get(playlistIndex);
-						controlador.reproducirCancion(cancionActual);
-						btnPlay.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/PauseButton.png")));
-						estadoReproductor = PLAYING;
-						lblSelectedSong.setText(cancionActual.getTitulo());
-						lblSelectedArtist.setText(cancionActual.getInterprete());
-						frame.repaint();
-						frame.revalidate();
-					}
-				}
-				
-			}
-		});
+		btnPrevious.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "PreviousButton.png")));
+		btnPrevious.addActionListener(this);
 		
 		Component rigidArea_2 = Box.createRigidArea(new Dimension(20, 20));
 		panel_1.add(rigidArea_2);
 		
 		btnPlay = new JButton("");
 		panel_1.add(btnPlay);
-		btnPlay.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/PlayButton.png")));
-		btnPlay.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				if (cancionActual != null) {
-					
-					if (estadoReproductor == PAUSED) {
-						controlador.reproducirCancion(cancionActual);
-						btnPlay.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/PauseButton.png")));
-						estadoReproductor = PLAYING;
-					} else if (estadoReproductor == PLAYING) {
-						controlador.pausarCancion();
-						btnPlay.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/PlayButton.png")));
-						estadoReproductor = PAUSED;
-					}
-					
-					
-				}
-			}
-		});
+		btnPlay.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "PlayButton.png")));
+		btnPlay.addActionListener(this);
 		
 		Component rigidArea_3 = Box.createRigidArea(new Dimension(20, 20));
 		panel_1.add(rigidArea_3);
 		
 		btnNext = new JButton("");
 		panel_1.add(btnNext);
-		btnNext.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/NextButton.png")));
-		btnNext.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				if (playlistActual != null) {
-					if (playlistIndex == playlistActual.getCanciones().size()-1) {
-						playlistIndex = 0;
-						cancionActual = playlistActual.getCanciones().get(playlistIndex);
-						controlador.reproducirCancion(cancionActual);
-						btnPlay.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/PauseButton.png")));
-						estadoReproductor = PLAYING;
-						lblSelectedSong.setText(cancionActual.getTitulo());
-						lblSelectedArtist.setText(cancionActual.getInterprete());
-						frame.repaint();
-						frame.revalidate();
-					} else {
-						playlistIndex++;
-						cancionActual = playlistActual.getCanciones().get(playlistIndex);
-						controlador.reproducirCancion(cancionActual);
-						btnPlay.setIcon(new ImageIcon(Principal.class.getResource("/umu/tds/apps/images/PauseButton.png")));
-						estadoReproductor = PLAYING;
-						lblSelectedSong.setText(cancionActual.getTitulo());
-						lblSelectedArtist.setText(cancionActual.getInterprete());
-						frame.repaint();
-						frame.revalidate();
-					}
-				}
-				
-				
-			}
-		});
+		btnNext.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "NextButton.png")));
+		btnNext.addActionListener(this);
 		
 		content_panel = new JPanel();
 		content_panel.setMaximumSize(new Dimension(975, 32767));
@@ -482,6 +299,161 @@ public class Principal {
 		current_content = content_panel;
 		enablePremiumFeatures();
 	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnLogout) {
+			controlador.logout();
+			Login windowL = new Login();
+			frame.dispose();
+		}
+		else if (e.getSource() == btnMejorar) {
+			scrollPane.setVisible(false);
+			MejorarCuenta panel_mc = new MejorarCuenta(frame);
+			body_panel.remove(current_content);
+			body_panel.add(panel_mc, BorderLayout.CENTER);
+			current_content = panel_mc;
+			frame.repaint();
+			frame.revalidate();
+		} 
+		else if (e.getSource() == btnExplorar) {
+			scrollPane.setVisible(false);
+			Explorar panel_exp = new Explorar(frame);
+			body_panel.remove(current_content);
+			body_panel.add(panel_exp, BorderLayout.CENTER);
+			current_content = panel_exp;
+			frame.repaint();
+			frame.revalidate();
+		} 
+		else if (e.getSource() == btnNuevaLista) {
+			scrollPane.setVisible(false);
+			NuevaLista panel_nl = new NuevaLista(frame);
+			body_panel.remove(current_content);
+			body_panel.add(panel_nl, BorderLayout.CENTER);
+			current_content = panel_nl;
+			frame.repaint();
+			frame.revalidate();
+		}
+		else if (e.getSource() == btnReciente) {
+			scrollPane.setVisible(false);
+			Reciente panel_rec = new Reciente(frame);
+			body_panel.remove(current_content);
+			body_panel.add(panel_rec, BorderLayout.CENTER);
+			current_content = panel_rec;
+			frame.repaint();
+			frame.revalidate();
+		}
+		else if (e.getSource() == btnMisListas) {
+			if (update_playlist_list()) {				// Existen listas
+				list.setSelectedIndex(0);
+				MisListas panel_ml = new MisListas(frame);
+				panel_ml.update_selected_playlist(list.getSelectedIndex());
+				body_panel.remove(current_content);
+				body_panel.add(panel_ml, BorderLayout.CENTER);
+				current_content = panel_ml;
+				frame.repaint();
+				frame.revalidate();
+			} else {
+				JOptionPane.showMessageDialog(frame,
+						"No existe ninguna playlist actualmente",
+						"Error", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		else if (e.getSource() == btnMasReproducidas) {
+			scrollPane.setVisible(false);
+			MasReproducidas panel_mr = new MasReproducidas(frame);
+			body_panel.remove(current_content);
+			body_panel.add(panel_mr, BorderLayout.CENTER);
+			current_content = panel_mr;
+			frame.repaint();
+			frame.revalidate();
+		}
+		else if (e.getSource() == btnImprimir) {
+			JFileChooser chooser = new JFileChooser();
+			chooser.setCurrentDirectory(new java.io.File("."));
+			chooser.setDialogTitle("Selecciona carpeta de destino");
+			chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			chooser.setAcceptAllFileFilterUsed(false);
+			
+			if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+				String path = chooser.getSelectedFile().getAbsolutePath();
+				System.out.println(path);
+				try {
+					controlador.printPDF(path);
+				} catch (DocumentException ev) {
+					ev.printStackTrace();
+				}
+			}
+		}
+		else if (e.getSource() == btnPrevious) {
+			if (playlistActual != null) {
+				if (playlistIndex == 0) {
+					playlistIndex = playlistActual.getCanciones().size()-1;
+					cancionActual = playlistActual.getCanciones().get(playlistIndex);
+					controlador.reproducirCancion(cancionActual);
+					btnPlay.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "PauseButton.png")));
+					estadoReproductor = PLAYING;
+					lblSelectedSong.setText(cancionActual.getTitulo());
+					lblSelectedArtist.setText(cancionActual.getInterprete());
+					frame.repaint();
+					frame.revalidate();
+				} else {
+					playlistIndex--;
+					cancionActual = playlistActual.getCanciones().get(playlistIndex);
+					controlador.reproducirCancion(cancionActual);
+					btnPlay.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "PauseButton.png")));
+					estadoReproductor = PLAYING;
+					lblSelectedSong.setText(cancionActual.getTitulo());
+					lblSelectedArtist.setText(cancionActual.getInterprete());
+					frame.repaint();
+					frame.revalidate();
+				}
+			}
+		}
+		else if (e.getSource() == btnPlay) {
+			if (cancionActual != null) {
+				if (estadoReproductor == PAUSED) {
+					controlador.reproducirCancion(cancionActual);
+					btnPlay.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "PauseButton.png")));
+					estadoReproductor = PLAYING;
+				} else if (estadoReproductor == PLAYING) {
+					controlador.pausarCancion();
+					btnPlay.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "PlayButton.png")));
+					estadoReproductor = PAUSED;
+				}
+				
+				
+			}
+		}
+		else if (e.getSource() == btnNext)  {
+			if (playlistActual != null) {
+				if (playlistIndex == playlistActual.getCanciones().size()-1) {
+					playlistIndex = 0;
+					cancionActual = playlistActual.getCanciones().get(playlistIndex);
+					controlador.reproducirCancion(cancionActual);
+					btnPlay.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "PauseButton.png")));
+					estadoReproductor = PLAYING;
+					lblSelectedSong.setText(cancionActual.getTitulo());
+					lblSelectedArtist.setText(cancionActual.getInterprete());
+					frame.repaint();
+					frame.revalidate();
+				} else {
+					playlistIndex++;
+					cancionActual = playlistActual.getCanciones().get(playlistIndex);
+					controlador.reproducirCancion(cancionActual);
+					btnPlay.setIcon(new ImageIcon(Principal.class.getResource(IMG_SRC + "PauseButton.png")));
+					estadoReproductor = PLAYING;
+					lblSelectedSong.setText(cancionActual.getTitulo());
+					lblSelectedArtist.setText(cancionActual.getInterprete());
+					frame.repaint();
+					frame.revalidate();
+				}
+			}
+		}
+	}
+	
+	
 	
 	
 	public boolean update_playlist_list() {
